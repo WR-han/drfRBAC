84ad9eb380df0484c6cca924e81a9d5d3dc56652
wrhan <wr.han@foxmail.com>
2021-03-16 14:48:02 +0800

    21.3.16

--- APPS/RBAC/apps.py
+++ APPS/RBAC/apps.py
@@ -1,7 +1,10 @@
 import inspect
+import os
 
 from django.apps import AppConfig
 from django.utils.module_loading import import_string, import_module
+
+from drfRBAC.settings import BASE_DIR
 
 
 class RbacConfig(AppConfig):
@@ -11,15 +14,21 @@ class RbacConfig(AppConfig):
         try:
             permissions_model = import_string("APPS.RBAC.models.Permissions")
             permissions_model.objects.get_or_create(name="管理员权限", codeName="AdminPermission")
-            permissions = import_module("Module_Auth.Permissions.RBAC_Permissions")
-            class_members = inspect.getmembers(permissions, inspect.isclass)
-            classes = [(c[0], c[1].__doc__.strip()) for c in class_members if
-                       c[0] not in ('MainPermission', 'SecondaryPermission')]
-            prefixes = [('GET', '获取'), ('PUT', '修改'), ('POST', '创建'), ('DELETE', '删除')]
-            for prefix in prefixes:
-                for Permission in classes:
-                    permissions_model.objects.get_or_create(name=f"{prefix[1]}{Permission[1]}",
-                                                            codeName=f"{prefix[0]}_{Permission[0]}")
-                    print(f"<{prefix[1]}{Permission[1]}> 权限创建成功")
+            permissions_dir = os.listdir(os.path.join(BASE_DIR, "Module_Auth", "Permissions"))
+            for permission_py in permissions_dir:
+                if ".py" in permission_py:
+                    permission_name = permission_py.split(".py")[0]
+                    permissions = import_module(f"Module_Auth.Permissions.{permission_name}")
+                    class_members = inspect.getmembers(permissions, inspect.isclass)
+                    print(class_members)
+                    classes = [(c[0], c[1].__doc__.strip()) for c in class_members if
+                               c[0] not in ('MainPermission', 'SecondaryPermission')]
+                    prefixes = [('GET', '获取'), ('PUT', '修改'), ('POST', '创建'), ('DELETE', '删除')]
+                    for prefix in prefixes:
+                        for Permission in classes:
+                            permissions_model.objects.get_or_create(name=f"{prefix[1]}{Permission[1]}",
+                                                                    codeName=f"{prefix[0]}_{Permission[0]}")
+                            print(f"<{prefix[1]}{Permission[1]}> 权限创建成功")
+                    print(f"---------------{permission_py} 模块内权限数据创建/检查完毕")
         except Exception as e:
             print(f"权限数据未创建，请先进行migrate操作,{e}")
